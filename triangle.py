9ca1f6ceb75544cfbf3db0b88798e2c8df04e56c
Deck4rdPy <basiluzumaki@mail.ru>
2025-09-24 13:28:21 +0300

    +if

--- triangle.py
+++ triangle.py
@@ -1,18 +1,19 @@
 from math import sqrt
 
-A = float(input('Enter A '))
-B = float(input('Enter B '))
-C = float(input('Enter C '))
+A = float(input('Enter A: '))
+B = float(input('Enter B: '))
+C = float(input('Enter C: '))
 
-if A < 0 or B < 0 or C < 0 and (A + B > C) and (A + C > B) and (B + C > A):
-    print('Wrong parameters ')
-    # Check the triangle inequality theorem
-
+# Check for negative or zero values
+if A <= 0 or B <= 0 or C <= 0:
+    print('Wrong parameters: sides must be positive numbers.')
 else:
-
-    P = A + B + C
-    p = P / 2
- #   S = (p * (p - A) * (p - B) * (p - C)) ** 0.5
-    S = sqrt((p * (p - A) * (p - B) * (p - C)))
-
-    print(f'Perimeter = {P} \nArea = {S}')
+    # Check the triangle inequality theorem
+    if (A + B > C) and (A + C > B) and (B + C > A):
+        P = A + B + C
+        p = P / 2
+        S = sqrt(p * (p - A) * (p - B) * (p - C))
+        print(f'Perimeter = {P}')
+        print(f'Area = {S}')
+    else:
+        print('Such a triangle does not exist.')
